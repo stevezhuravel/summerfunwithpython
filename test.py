7d89088b4a37d19d85d44463423a8f792e9c1fd1
stevezhuravel <33463234+stevezhuravel@users.noreply.github.com>
2019-08-01 17:00:56 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+from server import app
+import unittest
+
+class test(unittest.TestCase):
+    def testindex(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type = "html/text")
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
